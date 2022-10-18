af436249a5f17361f57db23c4e06d4cfd7be5298
Tim Hourihan <91994391+thourihan@users.noreply.github.com>
2022-10-18 15:40:16 -0400

    Update Grader.cpp

--- Grader.cpp
+++ Grader.cpp
@@ -1,28 +1,38 @@
 #include "Grader.h"
 
-double Grader::getCurrentGrade(vector<int> allGradesWeighted) {
-    // Make a variable maxGrade = 100
-    int maxGrade = 100;
+double Grader::getCurrentGrade() {
+    double tempGrade = 0;
     
+    // Add each weighted grade to the current grade variable
+    for (int i=0; i<this->allGradesWeighted.size(); i++){
+        tempGrade += this->allGradesWeighted[i];
+    }
+    
+    // Divide the current grade by the amount of grade values
+    tempGrade /= this->allGradesWeighted.size();
+    
+    // Result will be the student's current grade
+    return tempGrade;
+}
+
+double Grader::getMaxGradeAchievable() {
+    double maxGrade = 100;
+
     // For each value in the allGradesWeighted vector
-    for (int i=0; i < this->allGradesWeighted.size(); i++){
+    for (int i=0; i<this->allGradesWeighted.size(); i++){
         // Subtract 100 by weighted value of grade
         maxGrade -= allGradesWeighted[i];
     }
 
-    // Return maxGrade
-}
-
-double Grader::getMaxGradeAchievable(vector<int> allGradesWeighted) {
-    // Check if current size of allGradesWeighted is as big as it can be
-    // If it is not, then add 100s for each grade until allGradesWeighted
+    // Result will be the maximum possible grade
+    return maxGrade;
 }
 
 void Grader::startGrading() {
 
     // Get current grade
-    currentGrade = getCurrentGrade(this->allGradesWeighted);
+    currentGrade = getCurrentGrade();
 
     // Get max grade achievable
-    maxGradeAchievable = getMaxGradeAchievable(this->allGradesWeighted);
+    maxGradeAchievable = getMaxGradeAchievable();
 }
