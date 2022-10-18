3e837d10e59aa39a154806466dc90593a283de1a
Tim Hourihan <91994391+thourihan@users.noreply.github.com>
2022-10-18 15:31:08 -0400

    Update Grader.cpp

--- Grader.cpp
+++ Grader.cpp
@@ -1,23 +1,28 @@
 #include "Grader.h"
 
-double Grader::getCurrentGrade(vector<vector<int>> allGrades) {
+double Grader::getCurrentGrade(vector<int> allGradesWeighted) {
     // Make a variable maxGrade = 100
-    // For each value in the allGrades vector
+    int maxGrade = 100;
+    
+    // For each value in the allGradesWeighted vector
+    for (int i=0; i < this->allGradesWeighted.size(); i++){
         // Subtract 100 by weighted value of grade
-        
+        maxGrade -= allGradesWeighted[i];
+    }
+
     // Return maxGrade
 }
 
-double Grader::getMaxGradeAchievable(vector<vector<int>> allGrades) {
-    // Check if current size of allGrades is as big as it can be
-    // If it is not, then add 100s for each grade until allGrades
+double Grader::getMaxGradeAchievable(vector<int> allGradesWeighted) {
+    // Check if current size of allGradesWeighted is as big as it can be
+    // If it is not, then add 100s for each grade until allGradesWeighted
 }
 
 void Grader::startGrading() {
-    
+
     // Get current grade
-    currentGrade = getCurrentGrade(this->allGrades);
-    
+    currentGrade = getCurrentGrade(this->allGradesWeighted);
+
     // Get max grade achievable
-    maxGradeAchievable = getMaxGradeAchievable(this->allGrades);
+    maxGradeAchievable = getMaxGradeAchievable(this->allGradesWeighted);
 }
