0d0ca370686e5ecf331d2378adeb5a5fcf77b283
Henry Gutierrez <78116454+henrygutierrez@users.noreply.github.com>
2022-10-18 12:00:31 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,4 @@
-#include "Grader.h"
-
-
+#include "grader.h"
 
 int main() {
     // Create vectors for each category
@@ -11,8 +9,20 @@ int main() {
         // Prompt user how many grades they have
         // For each grade the user has to enter:
             // Add grade to its category vector
+    
+    // Call the class functions
+    // Print results
+    //order from brightspace
+    // labs -> assignments -> final project -> final exam -> midterm project
+    vector<int> assingments, labs;
+    int finalProject, midTermProject, finalExam, gradeSize;
 
-
-
+    for (int i = 0; i < 5; i++){
+        cout << "How many grades do you have for this category? : ";
+        cin >> gradeSize;
+        for(int j = 0; j < gradeSize; j++){
+            cout << "input as labs, assignments, final project, final exam, midterm project order";
+            
+        }
+    }
 }
-
