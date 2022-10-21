7517cd943e11e61ffe549a5d4ae8038ed9d28f36
Jagger-Pacheco <98493934+Jagger-Pacheco@users.noreply.github.com>
2022-10-21 13:57:20 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 void getInput(Grader &grader);
 void printResults(Grader &grader);
 
+
 int main() {
     // Create instance of class "Grader"
     Grader grader;
@@ -23,14 +24,19 @@ void getInput(Grader &grader){
     cout << "Input grade at each category" << endl;
     vector<string> categories = {"Labs", "Assignments", "Term Project", "Final Exam", "Review Project"};
 
-    // For each category 
+    // For each category
+    grader.termProject = -1;
+    grader.finalExam = -1;
+    grader.reviewProject = -1;
      for (int i = 0; i < categories.size(); i++){
         int temp = 0;
         cout << "How many grades do you have for " << categories[i] << std::endl;
 
         cin >> temp;
 
-        cout << "Enter your grade for: " << categories[i] << std::endl; 
+        if(temp > 0){
+            cout << "Enter your grade for: " << categories[i] << std::endl;
+        }
 
         for(int j = 0; j < temp; j++){
             double grades;
@@ -48,16 +54,19 @@ void getInput(Grader &grader){
             else if(categories[i] == "Term Project"){
                 cin >> grades;
                 grader.termProject = grades;
+                break;
             }
 
             else if(categories[i] == "Final Exam"){
                 cin >> grades;
                 grader.finalExam = grades;
+                break;
             }
 
             else if(categories[i] == "Review Project"){
                 cin >> grades;
                 grader.reviewProject = grades;
+                break;
             }
         }
         // cout enter grade for category[i]
@@ -69,9 +78,17 @@ void getInput(Grader &grader){
 
 void printResults(Grader &grader){
     // Display each property from the grader class to the user
-    cout << "Your current grade for the class: " << grader.currentGrade << endl;
+    cout << "Lab Grade: " << grader.lGrade << endl;
+    cout << "Assignment Grade: " << grader.aGrade << endl;
     // Display the student's current grade
-        // Show a breakdown of the student's grade by each assignment
-
+    cout << "Your current grade for the class: " << grader.currentGrade << endl;
+    // Display whether student is eligible to skip final exam with current grade
+    if(grader.currentGrade - (grader.reviewProject * 0.04) >= 90 && grader.finalExam == -1){
+        cout << "With your current grade you would be able to skip the final exam" << endl;
+    }
+    else if(grader.finalExam == -1){
+        cout << "With your current grade you would NOT be able to skip the final exam" << endl;
+    }
     // Show the maximum possible grade achievable if the student earns a 100 on everything in the future
+    cout << "Your maximum possible grade achievable for the class: " << grader.maxGradeAchievable << endl;
 }
